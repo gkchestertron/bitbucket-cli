a8f0c501864d8bcb59999e42227465592755e984
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2016-04-20 10:49:19 -0700

    improves branch command

--- commands/branch.js
+++ commands/branch.js
@@ -8,14 +8,23 @@ module.exports = {
         exec: function (target, branchNumber, index) {
             return exec("git branch -a | grep '"+branchNumber+"'")
             .then(function (branches) {
-                branches = _.map(branches.split('\n'), function (branch) {
-                    return branch.trim();
+                index = index || 0;
+                branches = _.filter(_.map(branches.split('\n'), function (branch) {
+                    return branch.split('*').join('').trim();
+                }), function (branch) {
+                    return branch.length;
                 });
 
-                if (branches.length === 1)
-                    return exec('git checkout '+branches[0]);
-                else if (index !== undefined)
-                    return exec('git checkout '+branches[index]);
+
+                if (branches.length === 1 || index) {
+                    var branch = branches[index].split('/');
+                    
+                    while(/remotes|origin/.test(branch[0]))
+                          branch.shift();
+
+                    branch = branch.join('/');
+                    return exec('git checkout '+branch);
+                }
                 else
                     return 'Multiple branches:\n'+branches.join('\n');
             });
